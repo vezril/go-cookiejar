45880a65713610d9c000db4481735d41009d6212
Calvin Ference <calvin.ference@linux.com>
2017-03-01 13:54:23 -0500

    Added thread safety

--- collections/stack/stack.go
+++ collections/stack/stack.go
@@ -13,76 +13,100 @@
 // resizes than a simple dynamic array/slice would allow.
 package stack
 
+import (
+    "sync"
+)
+
 // The size of a block of data
 const blockSize = 4096
 
 // Last in, first out data structure.
 type Stack struct {
-	size     int
-	capacity int
-	offset   int
+    size        int
+    capacity    int
+    offset      int
+    lock        sync.mutex
 
-	blocks [][]interface{}
-	active []interface{}
+    blocks [][]interface{}
+    active []interface{}
 }
 
 // Creates a new, empty stack.
 func New() *Stack {
-	result := new(Stack)
-	result.active = make([]interface{}, blockSize)
-	result.blocks = [][]interface{}{result.active}
-	result.capacity = blockSize
-	return result
+    result := new(Stack)
+    result.active = make([]interface{}, blockSize)
+    result.blocks = [][]interface{}{result.active}
+    result.capacity = blockSize
+    result.lock = sync.Mutex{}
+    return result
 }
 
 // Pushes a value onto the stack, expanding it if necessary.
 func (s *Stack) Push(data interface{}) {
-	if s.size == s.capacity {
-		s.active = make([]interface{}, blockSize)
-		s.blocks = append(s.blocks, s.active)
-		s.capacity += blockSize
-		s.offset = 0
-	} else if s.offset == blockSize {
-		s.active = s.blocks[s.size/blockSize]
-		s.offset = 0
-	}
-	s.active[s.offset] = data
-	s.offset++
-	s.size++
+    s.lock.Lock()
+    defer s.lock.Unlock()
+
+    if s.size == s.capacity {
+        s.active = make([]interface{}, blockSize)
+        s.blocks = append(s.blocks, s.active)
+        s.capacity += blockSize
+        s.offset = 0
+    } else if s.offset == blockSize {
+        s.active = s.blocks[s.size/blockSize]
+        s.offset = 0
+    }
+    s.active[s.offset] = data
+    s.offset++
+    s.size++
 }
 
 // Pops a value off the stack and returns it. Currently no shrinking is done.
 func (s *Stack) Pop() (res interface{}) {
-	s.size--
-	s.offset--
-	if s.offset < 0 {
-		s.offset = blockSize - 1
-		s.active = s.blocks[s.size/blockSize]
-	}
-	res, s.active[s.offset] = s.active[s.offset], nil
-	return
+    s.lock.Lock()
+    defer s.lock.Unlock()
+
+    s.size--
+    s.offset--
+    if s.offset < 0 {
+        s.offset = blockSize - 1
+        s.active = s.blocks[s.size/blockSize]
+    }
+    res, s.active[s.offset] = s.active[s.offset], nil
+    return
 }
 
 // Returns the value currently on the top of the stack. No bounds are checked.
 func (s *Stack) Top() interface{} {
-	if s.offset > 0 {
-		return s.active[s.offset-1]
-	} else {
-		return s.blocks[(s.size-1)/blockSize][blockSize-1]
-	}
+    s.lock.Lock()
+    defer s.lock.Unlock()
+
+    if s.offset > 0 {
+        return s.active[s.offset-1]
+    } else {
+        return s.blocks[(s.size-1)/blockSize][blockSize-1]
+    }
 }
 
 // Checks whether the stack is empty or not.
 func (s *Stack) Empty() bool {
-	return s.size == 0
+    s.lock.Lock()
+    defer s.lock.Unlock()
+
+    return s.size == 0
 }
 
 // Returns the number of elements in the stack.
 func (s *Stack) Size() int {
-	return s.size
+    s.lock.Lock()
+    defer s.lock.Unlock()
+
+    return s.size
 }
 
 // Resets the stack, effectively clearing its contents.
 func (s *Stack) Reset() {
-	*s = *New()
+    s.lock.Lock()
+    defer s.lock.Unlock()
+
+    *s = *New()
 }
