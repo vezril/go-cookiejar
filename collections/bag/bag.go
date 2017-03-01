85aafdba55c999970fa5bd5c3641a357efe2a68c
Calvin Ference <calvin.ference@linux.com>
2017-03-01 15:16:11 -0500

    Added concurrency to bag

--- collections/bag/bag.go
+++ collections/bag/bag.go
@@ -13,56 +13,73 @@
 // present in the bag.
 package bag
 
+import (
+    "sync"
+)
+
 // Bag data structure (multiset).
 type Bag struct {
-	size int
-	data map[interface{}]int
+    size    int
+    data    map[interface{}]int
+    lock    sync.Mutex
 }
 
 // Creates a new empty bag.
 func New() *Bag {
-	return &Bag{0, make(map[interface{}]int)}
+    return &Bag{0, make(map[interface{}]int), &sync.Mutex{}}
 }
 
 // Inserts an element into the bag.
 func (b *Bag) Insert(val interface{}) {
-	b.data[val]++
-	b.size++
+    lock.Lock()
+    defer lock.Unlock()
+    b.data[val]++
+    b.size++
 }
 
 // Removes an element from the bag. If none was present, nothing is done.
 func (b *Bag) Remove(val interface{}) {
-	old, ok := b.data[val]
-	if ok {
-		if old > 1 {
-			b.data[val] = old - 1
-		} else {
-			delete(b.data, val)
-		}
-		b.size--
-	}
+    lock.Lock()
+    defer lock.Unlock()
+    old, ok := b.data[val]
+    if ok {
+        if old > 1 {
+            b.data[val] = old - 1
+        } else {
+            delete(b.data, val)
+        }
+        b.size--
+    }
 }
 
 // Returns the total number of elemens in the bag.
 func (b *Bag) Size() int {
-	return b.size
+    lock.Lock()
+    defer lock.Unlock()
+    return b.size
 }
 
 // Counts the number of occurances of an element in the bag.
 func (b *Bag) Count(val interface{}) int {
-	return b.data[val]
+    lock.Lock()
+    defer lock.Unlock()
+    return b.data[val]
 }
 
 // Executes a function for every element in the bag.
 func (b *Bag) Do(f func(interface{})) {
-	for val, cnt := range b.data {
-		for ; cnt > 0; cnt-- {
-			f(val)
-		}
-	}
+    lock.Lock()
+    defer lock.Unlock()
+    for val, cnt := range b.data {
+        for ; cnt > 0; cnt-- {
+            f(val)
+        }
+    }
 }
 
 // Clears the contents of a bag.
 func (b *Bag) Reset() {
-	*b = *New()
+    lock.Lock()
+    defer lock.Unlock()
+    *b = *New()
 }
