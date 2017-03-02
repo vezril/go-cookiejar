e1164e81e7dfe3eb279f6462a15d810bc94b0d21
Calvin Ference <cference@inocybe.ca>
2017-03-02 15:08:10 -0500

    Added index reading to tape

--- collections/tape/tape.go
+++ collections/tape/tape.go
@@ -14,22 +14,35 @@ type Tape struct {
 }
 
 func New() *Tape {
-	t := new(Tape)
+  t := new(Tape)
+  t.size = blockSize
 	t.lock = sync.Mutex{}
 	t.cells = make([]byte, blockSize)
+
 	return t
 }
 
-func (t *Tape) Left() {
-  t.index = t.index++ % t.size
+func (t *Tape) Right() {
+  t.lock.Lock()
+  defer t.lock.Unlock()
+
+  t.index = (t.index + 1) % t.size
 }
 
-func (t *Tape) Right() {
-  if t.index == 0
+func (t *Tape) Left() {
+  t.lock.Lock()
+  defer t.lock.Unlock()
+
+  if t.index == 0 {
     t.index = t.size
+  }
   t.index--
 }
 
-func (t *Tape) Read() (data byte) {
+func (t *Tape) returnIndex() uint {
+  return t.index
+}
+
+func (t *Tape) Read() byte {
   return t.cells[t.index]
 }
